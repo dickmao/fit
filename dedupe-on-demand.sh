8c78247c36f714e92f9b9c6133df3ec27617127e
dickmao <rchiang@cs.stonybrook.edu>
2018-01-08 11:41:54 -0500

    this is not python

--- dedupe-on-demand.sh
+++ dedupe-on-demand.sh
@@ -7,7 +7,7 @@ ITEMDIR=/var/lib/scrapyd/items/${PROJECT}
 while [ 1 ] ; do
 #    for spider in $(scrapyd-client -t http://scrapyd:6800 spiders -p ${PROJECT} | tail -n +2) ; do
 # FIXME
-    for spider in newyork:
+    for spider in newyork
         if [ -d ${ITEMDIR}/$spider ]; then
            if ( [ ! -e ${ITEMDIR}/${spider}/digest ] && [ ! -z "$(find -L ${ITEMDIR}/$spider -name 'Marker.*\.json')" ] ) || ( [ -e ${ITEMDIR}/${spider}/digest ] && [[ ! -z $(find -L ${ITEMDIR}/${spider} -name 'Marker.*\.json' -cnewer ${ITEMDIR}/${spider}/digest) ]] ); then
                python ${WDIR}/dedupe.py --redis-host redis --corenlp-uri http://corenlp:9005 ${ITEMDIR}/${spider}
