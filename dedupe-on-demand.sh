ff07ef0be793443e12e8269d4eaf4005d7fb1637
dickmao <rchiang@cs.stonybrook.edu>
2018-03-28 14:20:46 -0400

    remove revisionist from newyork

--- dedupe-on-demand.sh
+++ dedupe-on-demand.sh
@@ -26,7 +26,7 @@ while [ 1 ] ; do
                if [ $spider == "listingsproject" ]; then
                    options="${options} --revisionist --payfor 28"
                elif [ ${CIRCLE_BRANCH:-} == "dev" ]; then
-                   options="${options} --revisionist --payfor 2"
+                   options="${options} --payfor 2"
                fi
                python ${WDIR}/dedupe.py --redis-host redis --corenlp-uri http://corenlp:9005 ${ITEMDIR}/${spider}$options
                once+=([$spider]=1)
