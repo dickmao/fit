2a641328aa9a2c8fdcf6096240edac3a4ed7c8a7
dickmao <rchiang@cs.stonybrook.edu>
2018-05-16 11:26:27 -0400

    try enabling joinery in absentia

--- get_data.py
+++ get_data.py
@@ -20,25 +20,26 @@ def download_s3(s3_client, bucket, dir=".", payfor=9):
     Markers = sorted([content['Key'] for content in response['Contents']], reverse=True)
     latest = get_datetime(Markers[0])
     for m in Markers:
-        within = False
         if not os.path.exists(m):
             response = s3_client.get_object(Bucket=bucket, Key=m)
             with open(join(dir, m), 'w') as fp:
                 fp.write(response['Body'].read())
 
-        with open(join(dir, m), 'r') as fp:
-            try:
-                url2dt = json.load(fp, object_hook=datetime_parser)
-                for url,dt0 in url2dt.iteritems():
-                    if (latest - dt0).days < payfor:
+        within = payfor == 0
+        if not within:
+            with open(join(dir, m), 'r') as fp:
+                try:
+                    url2dt = json.load(fp, object_hook=datetime_parser)
+                    for url,dt0 in url2dt.iteritems():
+                        if (latest - dt0).days < payfor:
+                            within = True
+                            break
+                except ValueError:
+                    fp.seek(0)
+                    dates = pickle.load(fp)
+                    if any((latest.date() - dt).days < payfor for dt in dates):
                         within = True
-                        break
-            except ValueError:
-                fp.seek(0)
-                dates = pickle.load(fp)
-                if any((latest.date() - dt).days < payfor for dt in dates):
-                    within = True
-                
+
         if within:
             body = "Data.{}".format(m.split(".", 1)[1])
             jsons.append(body) # reader.py readds the parent dir
