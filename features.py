6093a0de8e0a9f4f20dd89cb591ce7d6e71a2280
dickmao <rchiang@cs.stonybrook.edu>
2018-05-18 18:58:19 -0400

    keep investigating

--- features.py
+++ features.py
@@ -6,18 +6,15 @@ from corenlp import TimeoutException
 class CallableAnnotate(Callable):
     def __init__(self, client):
         self._client = client
-        self._foo = []
 
     def __call__(self, doc):
-        if self._foo:
-            return self._foo
         try:
             ann = self._client.annotate(doc, annotators="ner".split())
         except TimeoutException as e:
             print "TimeoutException: ", doc.encode('utf-8')
             return []
-        self._foo = itertools.chain.from_iterable([[t.lemma for t in s.token if t.pos != "CD"] for s in ann.sentence])
-        return self._foo
+        print doc[0:10]
+        return itertools.chain.from_iterable([[t.lemma for t in s.token if t.pos != "CD"] for s in ann.sentence])
 
 def get_text_length(docs):
     return np.array([len(doc) for doc in docs]).reshape(-1, 1)
